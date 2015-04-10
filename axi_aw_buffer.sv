8cc3ac2ab1eebb06b1313c06128de139d23662c5
Igor Loi <igor.loi@unibo.it>
2015-04-10 10:38:55 +0200

    Fixed Minor issues

--- axi_aw_buffer.sv
+++ axi_aw_buffer.sv
@@ -1,15 +1,14 @@
 module axi_aw_buffer
-  #(
+#(
     parameter ID_WIDTH = 4,
     parameter ADDR_WIDTH = 32,
     parameter USER_WIDTH = 6,
     parameter BUFFER_DEPTH = 2
-    )
-   (
-    
+)
+(
     input logic                   clk_i,
     input logic                   rst_ni,
-    
+
     input  logic                  slave_valid_i,
     input  logic [ADDR_WIDTH-1:0] slave_addr_i,
     input  logic [2:0]            slave_prot_i,
@@ -23,7 +22,7 @@ module axi_aw_buffer
     input  logic [ID_WIDTH-1:0]   slave_id_i,
     input  logic [USER_WIDTH-1:0] slave_user_i,
     output logic                  slave_ready_o,
-    
+
     output logic                  master_valid_o,
     output logic [ADDR_WIDTH-1:0] master_addr_o,
     output logic [2:0]            master_prot_o,
@@ -37,33 +36,32 @@ module axi_aw_buffer
     output logic [ID_WIDTH-1:0]   master_id_o,
     output logic [USER_WIDTH-1:0] master_user_o,
     input  logic                  master_ready_i
-    
-    );
-   
-   logic [30+ADDR_WIDTH+USER_WIDTH+ID_WIDTH-1:0] s_data_in;
-   logic [30+ADDR_WIDTH+USER_WIDTH+ID_WIDTH-1:0] s_data_out;
-   
-   
-   
-  assign s_data_in = {slave_cache_i,  slave_prot_i,  slave_lock_i,  slave_burst_i,  slave_size_i,  slave_len_i,  slave_qos_i,  slave_region_i,  slave_addr_i,  slave_user_i,  slave_id_i};
-  assign             {master_cache_o, master_prot_o, master_lock_o, master_burst_o, master_size_o, master_len_o, master_qos_o, master_region_o, master_addr_o, master_user_o, master_id_o} = s_data_out;
+);
 
-   
-   axi_buffer
-     #(
-       .DATA_WIDTH(30+ADDR_WIDTH+USER_WIDTH+ID_WIDTH),
-       .BUFFER_DEPTH(BUFFER_DEPTH)
-       )
-   buffer_i
-     (
-      .clk_i(clk_i),
-      .rst_ni(rst_ni),
-      .valid_o(master_valid_o),
-      .data_o(s_data_out),
-      .ready_i(master_ready_i),
-      .valid_i(slave_valid_i),
-      .data_i(s_data_in),
-      .ready_o(slave_ready_o)
-      );
-   
+    logic [29+ADDR_WIDTH+USER_WIDTH+ID_WIDTH-1:0] s_data_in;
+    logic [29+ADDR_WIDTH+USER_WIDTH+ID_WIDTH-1:0] s_data_out;
+
+
+
+    assign s_data_in = {slave_cache_i,  slave_prot_i,  slave_lock_i,  slave_burst_i,  slave_size_i,  slave_len_i,  slave_qos_i,  slave_region_i,  slave_addr_i,  slave_user_i,  slave_id_i};
+    assign             {master_cache_o, master_prot_o, master_lock_o, master_burst_o, master_size_o, master_len_o, master_qos_o, master_region_o, master_addr_o, master_user_o, master_id_o} = s_data_out;
+
+
+    axi_buffer
+    #(
+        .DATA_WIDTH(29+ADDR_WIDTH+USER_WIDTH+ID_WIDTH),
+        .BUFFER_DEPTH(BUFFER_DEPTH)
+    )
+    buffer_i
+    (
+        .clk_i(clk_i),
+        .rst_ni(rst_ni),
+        .valid_o(master_valid_o),
+        .data_o(s_data_out),
+        .ready_i(master_ready_i),
+        .valid_i(slave_valid_i),
+        .data_i(s_data_in),
+        .ready_o(slave_ready_o)
+    );
+
 endmodule
